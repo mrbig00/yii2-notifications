faff168a02f7074db3badc44225ea723ba8b8558
Szántó Zoltán <mrbig00@gmail.com>
2017-08-20 22:01:56 +0300

    Update NotificationsAsset.php

--- widgets/NotificationsAsset.php
+++ widgets/NotificationsAsset.php
@@ -15,12 +15,12 @@ class NotificationsAsset extends AssetBundle
     /**
      * @var string The assets source path
      */
-    public $sourcePath = '@vendor/machour/yii2-notifications/assets/';
+    public $sourcePath = '@vendor/mrbig00/yii2-notifications/assets/';
 
     /**
      * @var string The assets directory
      */
-    public static $assetsDirectory = '@vendor/machour/yii2-notifications/assets/';
+    public static $assetsDirectory = '@vendor/mrbig00/yii2-notifications/assets/';
 
     /**
      * @var array The widget js files
